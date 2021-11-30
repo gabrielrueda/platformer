65da38cd77249e08a665b484c3701365ddd72db9
Gabriel <ruedag@uwindsor.ca>
2021-11-30 10:25:36 -0500

    Added Platform and Character Jumping

--- Scripts/main.py
+++ Scripts/main.py
@@ -9,56 +9,119 @@ import os
 
 frameRate = 60
 windowSize = [1200,600]
+scale = 8
+windowPosition = [0,75]
 
 # defining colors
 black = pygame.Color(0, 0, 0)
-white = pygame.Color(255, 255, 255)
+darkPurple = pygame.Color(19,12,55)
 red = pygame.Color(255, 0, 0)
 green = pygame.Color(0, 255, 0)
 blue = pygame.Color(0, 0, 255)
 
 pygame.init()
 tileset = pygame.image.load(os.path.dirname(os.getcwd()) + '/platformer/Assets/tileset.png')
+skelSheet = pygame.image.load(os.path.dirname(os.getcwd()) + '/platformer/Assets/skeleton.png')
 # background = pygame.image.load(os.path.dirname(os.getcwd()) + '/platformer/Assets/preview.png')
-background = tileset.subsurface((112,33,64,31))
+
+background = pygame.transform.scale(tileset.subsurface((112,33,64,31)), (64*scale,31*scale))
+
 treeTop = tileset.subsurface((39,39,26,26))
-leftLedge = tileset.subsurface((72,24,7,7))
-regLedge = tileset.subsurface((80,24,7,7))
-regLedgePole = tileset.subsurface((87,24,10,7))
+
+leftLedge = pygame.transform.scale(tileset.subsurface((72,24,7,7)), (5*7, 5*7))
+
+regLedge = pygame.transform.scale(tileset.subsurface((80,24,7,7)), (5*7, 5*7))
+
+regLedgePole = pygame.transform.scale(tileset.subsurface((87,24,10,7)), (5*10, 5*7))
+
 rightLedge = tileset.subsurface((97,24,7,7))
+
 bothLedge = tileset.subsurface((96,32,8,8))
+
 treeTrunk = tileset.subsurface((66,48,4,16))
-treeTruckBottom = tileset.subsurface((72,56,8,8))
+
+treeTruckBottom = pygame.transform.scale(tileset.subsurface((72,56,8,8)), (5*8, 5*8))
+
 ladder = tileset.subsurface((72,32,8,8))
+
+skeleton = pygame.transform.scale(skelSheet.subsurface((5,25,10,14)), (5*10, 5*14))
+
+#The Grasses:
+grass_TL = pygame.transform.scale(tileset.subsurface((8,8,8,8)), (5*8, 5*8))
+grass_TC = pygame.transform.scale(tileset.subsurface((16,8,8,8)), (5*8, 5*8))
+grass_TR = pygame.transform.scale(tileset.subsurface((24,8,8,8)), (5*8, 5*8))
+
+grass_ML = pygame.transform.scale(tileset.subsurface((8,16,8,8)), (5*8, 5*8))
+grass_MC = pygame.transform.scale(tileset.subsurface((16,16,8,8)), (5*8, 5*8))
+grass_MR = pygame.transform.scale(tileset.subsurface((24,16,8,8)), (5*8, 5*8))
+
+grass_BL = pygame.transform.scale(tileset.subsurface((8,24,8,8)), (5*8, 5*8))
+grass_BC = pygame.transform.scale(tileset.subsurface((16,24,8,8)), (5*8, 5*8))
+grass_BR = pygame.transform.scale(tileset.subsurface((24,24,8,8)), (5*8, 5*8))
 
 game_window = pygame.display.set_mode(windowSize)
 pygame.display.set_caption('2D Platformer')
 fps = pygame.time.Clock()
 
+def bgAndWalkways():
+    # Clear canvas
+    game_window.fill(black)
+    # Generate Mountains
+    pygame.draw.rect(game_window, darkPurple, (0,0,windowSize[0],75), 0)
+    for i in range(0,4,1):
+        game_window.blit(background, (i*scale*64+windowPosition[0], windowPosition[1]))
+    #Generate Walkways
+    game_window.blit(treeTruckBottom, (375,260))
+    game_window.blit(grass_TC, (335,300))
+    game_window.blit(grass_TR, (375,300))
+    game_window.blit(grass_MR, (375,340))
+    game_window.blit(leftLedge,(415,300))
+    game_window.blit(regLedge,(450,300))
+    game_window.blit(regLedgePole,(485,300))
+    game_window.blit(regLedge,(485+50,300))
+
+
 def main():
-    scale = 9
-    windowPosition = [0,0]
+    start_ticks=pygame.time.get_ticks()
+    pos=229
+    jump = False
+    x = 450
+    speed = 0
     while True: 
+        seconds= round((pygame.time.get_ticks()-start_ticks)/1000,2)
+        g=170
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    windowPosition[0] -= 5
-                if event.key == pygame.K_LEFT and dir != 1:
-                    windowPosition[0] += 5
-                if event.key == pygame.K_UP and dir != 4:
-                    windowPosition[1] += 5
-                if event.key == pygame.K_DOWN and dir != 3:
-                    windowPosition[1] -= 5
-        img = pygame.transform.scale(background, (70*scale,32*scale))
-        game_window.fill(black)
-        
-        for i in range(0,4,1):
-            game_window.blit(img, (i*scale*70+windowPosition[0], windowPosition[1]))
-        
-        # game_window.blit(treeTop,(0,0))
+                    speed =5
+                if event.key == pygame.K_LEFT:
+                    speed = -5
+                if event.key == pygame.K_SPACE and pos >= 230:
+                    start_ticks=pygame.time.get_ticks()
+                    jump = True
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    speed = 0
+                if event.key == pygame.K_LEFT:
+                    speed = 0
+                    
+        x += speed
+
+
+        bgAndWalkways()
+        pos = g*(seconds*seconds) -200*seconds +230
+        if(pos <= 230 or jump == True):
+             game_window.blit(skeleton, (x, int(pos)))
+             jump = False
+        else:
+            game_window.blit(skeleton, (x, 231))
+            
+            
+        print(seconds)
+       
         pygame.display.flip()
         fps.tick(frameRate)
 
